19037959fecd20bd4dd6f5d9c523df1648744d3a
Tushar-programador. <Tusharkalra307@gmail.com.>
2024-09-09 10:42:39 +0530

    complte

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./.env" });
 const port = process.env.PORT || 5000;
 const DB_url = process.env.DATABASE_URL;
 
+
 const server = app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
